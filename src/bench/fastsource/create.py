be4fd80731d6d67ec3a1756e173f034ea3b92cd0
Gabe Fierro <gtfierro225@gmail.com>
2014-08-11 00:07:19 -0700

    create arbitrary numbers of fast sources for testing

---
+++ src/bench/fastsource/create.py
@@ -0,0 +1,33 @@
+from ConfigParser import RawConfigParser
+from StringIO import StringIO
+import sys
+import uuid
+
+configfile="""
+[report 0]
+ReportDeliveryLocation = http://localhost:8079/add
+
+[/]
+uuid = db43b080-176c-11e4-b2ab-6003089ed1d0
+
+[server]
+port = 8080
+
+[/fast]
+type = fast.Fast
+rate = .01
+"""
+
+s = StringIO(configfile)
+
+number = int(sys.argv[1])
+
+source = RawConfigParser()
+source.readfp(s)
+
+for i in xrange(number):
+    port = int(source.get('server','port'))
+    source.set('server', 'port', port+1)
+    source.set('/','uuid', str(uuid.uuid1()))
+    source.write(open('fast{0}.ini'.format(i), 'wb'))
+
