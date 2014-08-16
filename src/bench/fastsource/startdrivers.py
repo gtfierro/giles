3f59d52d0f371cd7a2b173411d01c589f8b201bd
Gabe Fierro <gtfierro225@gmail.com>
2014-08-15 20:36:51 -0700

    no stdout on fastsource

--- src/bench/fastsource/startdrivers.py
+++ src/bench/fastsource/startdrivers.py
@@ -1,7 +1,8 @@
 import subprocess
 import sys
 import glob
-
+import os
+FNULL = open(os.devnull, 'w')
 inis = glob.glob('*.ini')
 inis = [x.split('.')[0] for x in inis]
 
@@ -10,7 +11,7 @@ print inis
 processes = []
 for ini in inis:
     command = "twistd --pidfile {0}.pid -n smap {0}.ini".format(ini)
-    p = subprocess.Popen(command, shell=True)
+    p = subprocess.Popen(command, shell=True, stdout=FNULL)
     processes.append(p)
 # run smap query to see if the streams are registered
 # This is partly to get around the race condition arising when
