57095e0a5b5159654b5f5aac98a10f188a954e4b
Gabe Fierro <gtfierro225@gmail.com>
2014-09-08 14:16:15 -0700

    add some query tests

---
+++ src/archiver/query_test.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+/**
+type AST struct {
+	QueryType  QueryType_T
+	TargetType TargetType_T
+	Target     Target_T
+	Where      *Node
+}
+*/
+
+func TestSimple(t *testing.T) {
+	var query string
+	var ast *AST
+
+	var tt_type = reflect.TypeOf(&TagsTarget{})
+	var dt_type = reflect.TypeOf(&DataTarget{})
+	var st_type = reflect.TypeOf(&SetTarget{})
+
+	query = "select *"
+	ast = parse(query)
+	if ast.QueryType != SELECT_TYPE {
+		t.Error(query, "\nshould have query type SELECT_TYPE, but has type", ast.QueryType)
+	}
+	if ast.TargetType != TAGS_TARGET {
+		t.Error(query, "\nshould have target type TAGS_TARGET, but has type", ast.TargetType)
+	}
+	if reflect.TypeOf(ast.Target) != tt_type {
+		t.Error(query, "\nshould have target", tt_type, "but has target", reflect.TypeOf(ast.Target))
+	}
+
+	query = "select data before now where has uuid"
+	ast = parse(query)
+	if ast.QueryType != SELECT_TYPE {
+		t.Error(query, "\nshould have query type SELECT_TYPE, but has type", ast.QueryType)
+	}
+	if ast.TargetType != DATA_TARGET {
+		t.Error(query, "\nshould have target type DATA_TARGET, but has type", ast.TargetType)
+	}
+	if reflect.TypeOf(ast.Target) != dt_type {
+		t.Error(query, "\nshould have target", dt_type, "but has target", reflect.TypeOf(ast.Target))
+	}
+
+	query = "set Metadata/XYZ = 4 where has uuid"
+	ast = parse(query)
+	if ast.QueryType != SET_TYPE {
+		t.Error(query, "\nshould have query type SET_TYPE, but has type", ast.QueryType)
+	}
+	if ast.TargetType != SET_TARGET {
+		t.Error(query, "\nshould have target type SET_TARGET, but has type", ast.TargetType)
+	}
+	if reflect.TypeOf(ast.Target) != st_type {
+		t.Error(query, "\nshould have target", st_type, "but has target", reflect.TypeOf(ast.Target))
+	}
+}
