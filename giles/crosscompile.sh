ce5b49ee2d4fb1aa79c997e4959f22d71cc7ea85
Gabe Fierro <gtfierro225@gmail.com>
2014-09-26 14:35:32 -0700

    fix crosscompile script to work with new directory structure/naming

--- giles/crosscompile.sh
+++ giles/crosscompile.sh
@@ -1,9 +1,9 @@
-rm archiver-*
+rm giles-*
 PLATFORMS="darwin/386 darwin/amd64 linux/386 linux/amd64"
 for platform in $PLATFORMS ; do
 GOOS=${platform%/*}
 GOARCH=${platform#*/}
 echo "building $GOOS-$GOARCH"
 go build
-mv archiver archiver-$GOOS-$GOARCH
+mv giles giles-$GOOS-$GOARCH
 done
