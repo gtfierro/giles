c7fae24fe8d3d88f4460ecf08779eb6e429aace5
Gabe Fierro <gtfierro225@gmail.com>
2014-10-28 10:01:32 -0700

    restrict formsubmission code to each individual configuration -- bugfix

--- bench/bench.go
+++ bench/bench.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-  "fmt"
-  "net/http"
-  "runtime"
-  "log"
+	"fmt"
+	"log"
+	"net/http"
+	"runtime"
 
-  "sync"
-  "io"
-  "sync/atomic"
+	"io"
+	"sync"
+	"sync/atomic"
 
-  "bytes"
+	"bytes"
 )
 
 var data = []byte(`{"/sensor0" : {"Metadata" : {"SourceName" : "Test Source","Location" : { "City" : "Berkeley" }},"Properties": {"Timezone": "America/Los_Angeles","UnitofMeasure": "Watt","ReadingType": "double"},"Readings" : [[0, 0], [1, 1]],"uuid" : "d24325e6-1d7d-11e2-ad69-a7c2fa8dba61"}}`)
@@ -19,30 +19,30 @@ var bad uint64
 var gud uint64
 
 func makePost(s *sync.WaitGroup) {
-  defer s.Done()
-  var postdata io.Reader
-  postdata = bytes.NewBuffer(data)
+	defer s.Done()
+	var postdata io.Reader
+	postdata = bytes.NewBuffer(data)
 
-  _, err := http.Post("http://localhost:8079/add", "application/json", postdata)
-  if err != nil {
-    atomic.AddUint64(&bad, 1)
-    log.Println(err)
-  } else {
-    atomic.AddUint64(&gud, 1)
-  }
-  //resp.Body.Close()
+	_, err := http.Post("http://localhost:8079/add", "application/json", postdata)
+	if err != nil {
+		atomic.AddUint64(&bad, 1)
+		log.Println(err)
+	} else {
+		atomic.AddUint64(&gud, 1)
+	}
+	//resp.Body.Close()
 }
 
 func main() {
-  runtime.GOMAXPROCS(runtime.NumCPU())
+	runtime.GOMAXPROCS(runtime.NumCPU())
 
-  num := 1
-  wg.Add(num)
-  for x:=0; x<num; x+=1 {
-    makePost(&wg)
-  }
-  wg.Wait()
-  fmt.Println("bad", bad)
-  fmt.Println("gud", gud)
+	num := 1
+	wg.Add(num)
+	for x := 0; x < num; x += 1 {
+		makePost(&wg)
+	}
+	wg.Wait()
+	fmt.Println("bad", bad)
+	fmt.Println("gud", gud)
 
 }
