fc0df1f8706e994a9413e21e637aa1b2a968dbd9
Gabe Fierro <gtfierro225@gmail.com>
2014-10-28 10:09:38 -0700

    move handlers into their own file

--- giles/archiver.go
+++ giles/archiver.go
@@ -2,9 +2,7 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"github.com/gorilla/mux"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -23,104 +21,6 @@ var cm *ConnectionMap
 var incomingcounter = NewCounter()
 var pendingwritescounter = NewCounter()
 
-/**
- * Handles POSTing of new data
- * The handleJSON method parses the message received from the sMAP drivers
- * and delivers them as an array. Because metadata is delivered as k/v pairs
- * representing a tree, we have a pre-loop that stores the metadata values at
- * the higher levels of the tree. Then, when we loop through the data to add it
- * to the leaves of the tree (the actual timeseries), we query the prefixes
- * of the timeseries path to get all the 'trickle down' metadata from the higher
- * parts of the metadata tree. That logic takes place in store.SavePathMetadata and
- * store.SaveMetadata
-**/
-func AddReadingHandler(rw http.ResponseWriter, req *http.Request) {
-	//TODO: add transaction coalescing
-	defer req.Body.Close()
-	//TODO: check we have permission to write
-	//vars := mux.Vars(req)
-	//apikey := vars["key"]
-	messages, err := handleJSON(req.Body)
-	if err != nil {
-		log.Println(err)
-		rw.WriteHeader(500)
-		rw.Write([]byte(err.Error()))
-		return
-	}
-	incomingcounter.Mark()
-	//ok, err := store.CheckKey(apikey, messages)
-	//if err != nil {
-	//	log.Println(err)
-	//	rw.WriteHeader(500)
-	//	rw.Write([]byte(err.Error()))
-	//	return
-	//}
-	//if !ok {
-	//	rw.WriteHeader(400)
-	//	rw.Write([]byte("Unauthorized api key " + apikey))
-	//	return
-	//}
-	store.SavePathMetadata(&messages)
-	for _, msg := range messages {
-		go tsdb.Add(msg.Readings)
-		go store.SaveMetadata(msg)
-		go republisher.Republish(msg)
-	}
-	rw.WriteHeader(200)
-}
-
-/**
- * Receives POST request which contains metadata query. Subscribes the
- * requester to readings from streams which match that metadata query
-**/
-func RepublishHandler(rw http.ResponseWriter, req *http.Request) {
-	defer req.Body.Close()
-	stringquery, err := ioutil.ReadAll(req.Body)
-	if err != nil {
-		fmt.Println(err)
-	}
-	republisher.HandleSubscriber(rw, string(stringquery))
-}
-
-/**
- * Resolves sMAP queries and returns results
-**/
-func QueryHandler(rw http.ResponseWriter, req *http.Request) {
-	defer req.Body.Close()
-	vars := mux.Vars(req)
-	key := vars["key"]
-	stringquery, err := ioutil.ReadAll(req.Body)
-	if err != nil {
-		fmt.Println(err)
-	}
-	res, err := store.Query(stringquery, key)
-	if err != nil {
-		log.Println(err)
-		rw.WriteHeader(500)
-		rw.Write([]byte(err.Error()))
-		return
-	}
-	rw.WriteHeader(200)
-	rw.Write(res)
-}
-
-/**
- * Returns metadata for a uuid. A limited GET alternative to the POST query handler
-**/
-func TagsHandler(rw http.ResponseWriter, req *http.Request) {
-	vars := mux.Vars(req)
-	uuid := vars["uuid"]
-	rw.Header().Set("Content-Type", "application/json")
-	res, err := store.TagsUUID(uuid)
-	if err != nil {
-		log.Println(err)
-		rw.WriteHeader(500)
-		rw.Write([]byte(err.Error()))
-		return
-	}
-	rw.WriteHeader(200)
-	rw.Write(res)
-}
 
 // config flags
 var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
