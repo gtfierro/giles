9d5bf8470ff2d8414666f24cebeae2624cabbd40
Gabe Fierro <gtfierro225@gmail.com>
2014-11-08 00:49:16 -0600

    add function templates for eventual giles library

---
+++ giles/giles.go
@@ -0,0 +1,29 @@
+package main
+
+func AddData(readings []interface{}) bool {
+	return true
+}
+
+func GetData(streamids []string, start, end uint64) ([]interface{}, error) {
+	return []interface{}{}, nil
+}
+
+func PrevData(streamids []string, start uint64, limit int32) ([]interface{}, error) {
+	return []interface{}{}, nil
+}
+
+func NextData(streamids []string, start uint64, limit int32) ([]interface{}, error) {
+	return []interface{}{}, nil
+}
+
+func GetTags(select_tags, where_tags map[string]interface{}) (map[string]interface{}, error) {
+	return make(map[string]interface{}), nil
+}
+
+func GetUUIDs(where_tags map[string]interface{}) ([]string, error) {
+	return []string{}, nil
+}
+
+func SetTags(update_tags, where_tags map[string]interface{}) (int, error) {
+	return 0, nil
+}
