71d03c68605b1013fc3521c502e5ae9528a865b6
Gabe Fierro <gtfierro225@gmail.com>
2014-11-12 12:02:22 -0800

    move crosscompile script

--- crosscompile.sh
+++ crosscompile.sh
@@ -4,6 +4,6 @@ for platform in $PLATFORMS ; do
 GOOS=${platform%/*}
 GOARCH=${platform#*/}
 echo "building $GOOS-$GOARCH"
-go build
-mv giles giles-$GOOS-$GOARCH
+go build -o archiver
+mv archiver archiver-$GOOS-$GOARCH
 done
