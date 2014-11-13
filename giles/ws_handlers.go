3dd6146279a41db6ed171b00f7396f4f31f2cdb9
Gabe Fierro <gtfierro225@gmail.com>
2014-11-12 16:49:11 -0800

    add reading handler for ws

--- giles/ws_handlers.go
+++ giles/ws_handlers.go
@@ -11,6 +11,31 @@ var upgrader = &websocket.Upgrader{
 	WriteBufferSize: 1024,
 	CheckOrigin:     func(r *http.Request) bool { return true }}
 
+func WsAddReadingHandler(a *Archiver, rw http.ResponseWriter, req *http.Request) {
+	ws, err := upgrader.Upgrade(rw, req, nil)
+	if err != nil {
+		log.Error("Error establishing websocket: %v", err)
+		return
+	}
+	msgtype, msg, err := ws.ReadMessage()
+	vars := mux.Vars(req)
+	apikey := unescape(vars["key"])
+	messages, err := handleJSON(req.Body)
+	if err != nil {
+		log.Error("Error handling JSON: %v", err)
+		rw.WriteHeader(500)
+		rw.Write([]byte(err.Error()))
+		return
+	}
+	log.Debug("msgtype: %v, msg: %v, err: %v", msgtype, msg, err)
+	err = a.AddData(messages, apikey)
+	if err != nil {
+		ws.WriteJSON(map[string]string{"error": err.Error()})
+		return
+	}
+	ws.WriteJSON(map[string]string{"status": "ok"})
+}
+
 func WsTagsHandler(a *Archiver, rw http.ResponseWriter, req *http.Request) {
 	ws, err := upgrader.Upgrade(rw, req, nil)
 	if err != nil {
