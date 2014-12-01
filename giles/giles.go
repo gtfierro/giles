44d28422ceeaeab0debbe5c1a8f095c622364a7f
Gabe Fierro <gtfierro225@gmail.com>
2014-12-01 12:03:51 -0800

    construct a config object to start archiver

--- giles/giles.go
+++ giles/giles.go
@@ -6,10 +6,10 @@ import (
 	"github.com/gtfierro/giles/httphandler"
 	"github.com/gtfierro/giles/wshandler"
 	"log"
+	"net"
 	"os"
 	"runtime"
 	"runtime/pprof"
-	"strconv"
 	"time"
 )
 
@@ -18,9 +18,9 @@ var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
 var memprofile = flag.String("memprofile", "", "write memory profile to this file")
 var archiverport = flag.Int("port", 8079, "archiver service port")
 var readingdbip = flag.String("rdbip", "localhost", "ReadingDB IP address")
-var readingdbport = flag.Int("rdbport", 4242, "ReadingDB Port")
+var readingdbport = flag.String("rdbport", "4242", "ReadingDB Port")
 var mongoip = flag.String("mongoip", "localhost", "MongoDB IP address")
-var mongoport = flag.Int("mongoport", 27017, "MongoDB Port")
+var mongoport = flag.String("mongoport", "27017", "MongoDB Port")
 var tsdbstring = flag.String("tsdb", "readingdb", "Type of timeseries database to use: 'readingdb' or 'quasar'")
 var tsdbkeepalive = flag.Int("keepalive", 30, "Number of seconds to keep TSDB connection alive per stream for reads")
 var benchmarktimer = flag.Int("benchmark", 60, "Number of seconds to benchmark before quitting and writing profiles")
@@ -55,9 +55,20 @@ func main() {
 
 	runtime.GOMAXPROCS(runtime.NumCPU())
 
-	a := archiver.NewArchiver(*archiverport, *readingdbip, *readingdbport,
-		*mongoip, *mongoport, *tsdbstring, *tsdbkeepalive,
-		"0.0.0.0:"+strconv.Itoa(*archiverport))
+	tsdbaddress, err := net.ResolveTCPAddr("tcp4", *readingdbip+":"+*readingdbport)
+	if err != nil {
+		log.Println("Error resolving TCP address for TSDB", *readingdbip+":"+*readingdbport)
+	}
+	mongoaddress, err := net.ResolveTCPAddr("tcp4", *mongoip+":"+*mongoport)
+	if err != nil {
+		log.Println("Error resolving TCP address for mongo", *mongoip+":"+*mongoport)
+	}
+	config := &archiver.Config{Port: *archiverport,
+		TSDB:         *tsdbstring,
+		TSDBAddress:  tsdbaddress,
+		MongoAddress: mongoaddress,
+		Keepalive:    *tsdbkeepalive}
+	a := archiver.NewArchiver(config)
 	go a.PrintStatus()
 	httphandler.Handle(a)
 	wshandler.Handle(a)
