39ddd349259941518b2cb64999124c2a4dd6606c
Gabe Fierro <gtfierro225@gmail.com>
2014-12-04 11:42:45 -0800

    fix crosscompile script

--- crosscompile.sh
+++ crosscompile.sh
@@ -1,9 +1,11 @@
+cd giles
 rm giles-*
 PLATFORMS="darwin/386 darwin/amd64 linux/386 linux/amd64"
 for platform in $PLATFORMS ; do
 GOOS=${platform%/*}
 GOARCH=${platform#*/}
 echo "building $GOOS-$GOARCH"
-go build -o archiver
-mv archiver archiver-$GOOS-$GOARCH
+go build
+mv giles giles-$GOOS-$GOARCH
 done
+cd ..
