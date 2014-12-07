bb2a298dc36ef845d13e9f99d61602059ef0b72d
Gabe Fierro <gtfierro225@gmail.com>
2014-12-07 14:24:49 -0800

    add pycurl

--- gilescmd/setup.py
+++ gilescmd/setup.py
@@ -8,7 +8,7 @@ setup(name="gilescmd",
       author="Gabe Fierro",
       author_email="fierro@eecs.berkeley.edu",
       url="http://github.com/gtfierro/giles",
-      requires=["argparse", "pandas", "pymongo", "smap"],
-      install_requires=["argparse", "pandas", "pymongo", "smap"],
+      requires=["argparse", "pandas", "pymongo", "smap","pycurl"],
+      install_requires=["argparse", "pandas", "pymongo", "smap","pycurl"],
       scripts=["gilescmd"]
 )
