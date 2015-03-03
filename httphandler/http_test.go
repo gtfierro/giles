dbf9baef0e191641763bfa7ff8e24fa871fe018d
Gabe Fierro <gtfierro225@gmail.com>
2015-03-02 20:58:15 -0800

    add another benchmark to http

--- httphandler/http_test.go
+++ httphandler/http_test.go
@@ -27,10 +27,49 @@ func BenchmarkAddReading1(b *testing.B) {
 			"uuid": "b86df176-6b40-5d58-8f29-3b85f5cfbf1e"
 		}
 	}`
-	api := ":asdf"
+	api := "asdf"
 	url := fmt.Sprintf("http://localhost:%v/add/%v", *config.HTTP.Port, api)
 	for i := 0; i < b.N; i++ {
 		readershort := strings.NewReader(jsonstringshort)
 		http.Post(url, "application/json", readershort)
+	}
+}
+
+func BenchmarkAddReadingFull1(b *testing.B) {
+	var jsonstring = `
+	{
+		"/": {
+			"Contents": [
+				"fast"
+			]
+		},
+		"/fast": {
+			"Contents": [
+				"sensor0"
+			]
+		},
+		"/fast/sensor0": {
+			"Properties": {
+				"ReadingType": "long",
+				"Timezone": "America/Los_Angeles",
+				"UnitofMeasure": "V",
+				"UnitofTime": "s"
+			},
+			"Metadata": {
+				"Site": "Test Site",
+				"Nested": {
+					"key": "value",
+					"other": "value"
+				}
+			},
+			"Readings": [[9182731928374, 30]],
+			"uuid": "b86df176-6b40-5d58-8f29-3b85f5cfbf1e"
+		}
+	}`
+	api := "asdf"
+	url := fmt.Sprintf("http://localhost:%v/add/%v", *config.HTTP.Port, api)
+	for i := 0; i < b.N; i++ {
+		reader := strings.NewReader(jsonstring)
+		http.Post(url, "application/json", reader)
 	}
 }
