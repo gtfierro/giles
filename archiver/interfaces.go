f46df206f22ea7a21f07b7c6f9c64c3e8b8382e3
Gabe Fierro <gtfierro225@gmail.com>
2015-03-04 12:02:26 -0800

    add MetadataStore interface draft

--- archiver/interfaces.go
+++ archiver/interfaces.go
@@ -30,3 +30,28 @@ type TSDB interface {
 	// Adds a pointer to metadata store for streamid/uuid conversion and the like
 	AddStore(*Store)
 }
+
+// The metadata store should support the following operations
+type MetadataStore interface {
+	// Returns true if the key @apikey is allowed to write to each of the
+	// streams listed in @messages. Should check each SmapMessage.UUID value.
+	CheckKey(apikey string, messages map[string]*SmapMessage) (bool, error)
+	// Commits the metadata contained in each SmapMessage to the metadata
+	// store. Should consult the following properties of SmapMessage:
+	// Properties, Metadata, Actuator
+	SaveMetadata(messages map[string]*SmapMessage)
+	// Retrieves the tags indicated by @target for documents that match the
+	// @where clause. If @is_distinct is true, then it will return a list of
+	// distinct values for the tag @distinct_key
+	GetTags(target Dict, is_distinct bool, distinct_key string, where Dict) ([]interface{}, error)
+	// For all documents that match the where clause @where, apply the updates
+	// contained in @updates, provided that the key @apikey is valid for all of
+	// them
+	SetTags(updates Dict, apikey string, where Dict) (Dict, error)
+	// Returns all metadata for a given UUID
+	TagsUUID(uuid string) (Dict, error)
+	// Resolve a where clause to a slice of UUIDs
+	GetUUIDs(where Dict) ([]string, error)
+	// Returns the unit of time for the stream identified by the given UUID.
+	GetUnitOfTime(uuid string) UnitOfTime
+}
