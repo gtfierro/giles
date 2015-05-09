5758c747e0a75248d28a863eea640eb7eaf0e49c
Gabe Fierro <gtfierro225@gmail.com>
2015-05-08 19:17:04 -0700

    fix goprotobuf import to github.com/golang/protobuf

--- archiver/tsdb_readingdb.go
+++ archiver/tsdb_readingdb.go
@@ -1,7 +1,7 @@
 package archiver
 
 import (
-	"code.google.com/p/goprotobuf/proto"
+	"github.com/golang/protobuf/proto"
 	"encoding/binary"
 	rdbp "github.com/gtfierro/giles/internal/readingdbproto"
 	"net"
